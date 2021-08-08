21ef9de7ab5b1de42c6c58eab02339a9b9e3dddd
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-08-08 12:45:55 +0300

    Add webintro tests

---
+++ src/test/java/webintro/WebClass.java
@@ -0,0 +1,21 @@
+package webintro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class WebClass {
+    static WebDriver driver = new ChromeDriver();
+
+    public static void main(String[] args) {
+
+        driver.get("https://www.google.com");
+        driver.navigate().to("https://mail.ru");
+        driver.navigate().back();
+        driver.navigate().refresh();
+        driver.close();
+        driver.quit();
+    }
+}
