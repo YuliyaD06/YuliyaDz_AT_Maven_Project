822dde0b14d89cf32a7d895ab465533b50cfeb95
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-08-08 12:44:49 +0300

    Add test

---
+++ src/test/java/JUnit4Runner.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        MyFirstMavenTest1.class,
+        MyFirstMavenTest.class})
+
+public class JUnit4Runner {
+
+
+
+}
