523893cceabec13f59366003b4f50b7020d9def2
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-08-14 21:06:38 +0300

    Update WebDriver homework

--- src/test/java/webdriver/MouseHoverTest.java
+++ src/test/java/webdriver/MouseHoverTest.java
@@ -1,16 +1,11 @@
 package webdriver;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import java.util.concurrent.TimeUnit;
 //Задание:
 //1. Перейти на сайт https://booking.com
 //2. Навести мышь на индикатор валюты
@@ -18,13 +13,14 @@ import java.util.concurrent.TimeUnit;
 //4. Навести мышь на индикатор языка
 //5. Проверить значение подсказки
 public class MouseHoverTest {
-    WebDriver driver;
 
-    @Before
-    public void makePreconditions(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    private static WebDriver driver;
+
+
+    @BeforeClass
+    public static void makePreconditions(){
+
+        driver = ConditionsClass.getDriver();
         driver.get("https://www.booking.com/");
     }
 
@@ -47,8 +43,8 @@ public class MouseHoverTest {
         Assert.assertEquals("Choose your language", languageElement.getAttribute("data-tooltip-text"));
     }
 
-    @After
-    public void makePostConditions(){
+    @AfterClass
+    public static void makePostConditions(){
         driver.close();
         driver.quit();
     }
